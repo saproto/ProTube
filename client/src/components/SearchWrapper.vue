c2803de635f24853338a51daf4370284f9a3309c
Max Liebe <amalox@outlook.com>
2022-12-09 14:22:25 +0100

    prettier? more like uglier! take that!

--- client/src/components/SearchWrapper.vue
+++ client/src/components/SearchWrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <HeaderField>
-    <div class="flex-1 flex flex-col items-stretch">
+    <div class="flex flex-1 flex-col items-stretch">
       <h3
         class="flex w-full text-2xl font-medium leading-6 text-white md:block">
         <span class="w-full"> ProTube playlist panel</span>
@@ -13,7 +13,9 @@
       <div class="mt-2 max-w-xl text-sm text-gray-200">
         <p>Search for any song on YouTube and add it to the ProTube playlist</p>
       </div>
-      <form @submit.prevent="processQuery" class="mt-auto p-0 flex sm:items-center">
+      <form
+        @submit.prevent="processQuery"
+        class="mt-auto flex p-0 sm:items-center">
         <div class="group flex h-10 w-full md:max-w-md">
           <input
             minlength="1"
