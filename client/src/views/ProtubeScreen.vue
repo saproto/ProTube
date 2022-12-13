10e9a33c724fbaea976bdc87bc3e19fa212fb782
ysbrandB <ysbrand@burgstede.eu>
2022-12-14 00:14:37 +0100

    implement progress bar on currently playing

--- client/src/views/ProtubeScreen.vue
+++ client/src/views/ProtubeScreen.vue
@@ -29,10 +29,10 @@
         playerState.playerType === enums.TYPES.VIDEO &&
         playerState.playerMode !== enums.MODES.IDLE
       ">
-      <div did="playerID" class="min-h-screen w-full" />
+      <div :id="playerID" class="min-h-screen w-full" />
     </div>
   </div>
-  <div class="absolute bottom-0 mb-1 w-screen rounded-lg opacity-80">
+  <div class="absolute bottom-0 mb-1 w-screen rounded-lg">
     <div class="flex justify-between">
       <div
         class="border-proto_blue dark:bg-proto_secondary_gray-dark ml-4 mb-1 max-w-[10%] rounded-sm rounded-lg border-l-4 bg-white p-1 px-4 py-2 font-medium text-gray-900 opacity-80 shadow-lg ring-1 ring-black ring-opacity-5 dark:text-gray-50">
@@ -49,6 +49,7 @@
         :video="video"
         :index="index"
         :key="video.id"
+        :id="index === 0 ? 'current-video' : ''"
         class="mb-1 inline-block w-1/5 rounded-lg rounded-sm p-1">
         <div
           :style="{ background: `url(${video.thumbnail.url})` }"
@@ -57,11 +58,14 @@
             background-size: cover;
             background-position: center center;
           "
-          class="border-proto_blue group col-span-1 inline-block flex h-full w-full flex-1 rounded-sm rounded-lg border-l-4 text-center shadow">
+          class="border-proto_blue group relative col-span-1 inline-block flex h-full w-full flex-1 overflow-hidden rounded-sm rounded-lg border-l-4 text-center shadow">
           <div
-            class="rounded-m flex w-full flex-col rounded-lg rounded-sm border-t border-b border-r border-gray-400 bg-white/70 px-8 py-4 duration-200 dark:border-gray-800/80 dark:bg-stone-800/80">
+            :id="index === 0 ? 'progress-bar' : ''"
+            class="absolute h-full bg-white opacity-70" />
+          <div
+            class="rounded-m relative flex w-full flex-col rounded-lg rounded-sm border-t border-b border-r border-gray-400 bg-white/70 px-8 py-4 duration-200 dark:border-gray-800/80 dark:bg-stone-800/80">
             <h3
-              class="text-md h-[2rem] truncate text-left font-bold text-gray-800 dark:text-stone-300">
+              class="text-md z-1 h-[2rem] truncate text-left font-bold text-gray-800 dark:text-stone-300">
               {{ video.title }}
             </h3>
             <ul
@@ -121,6 +125,9 @@ import YoutubePlayer from "youtube-player";
 import enums from "@/js/Enums";
 
 const playerID = "player-" + Math.random();
+const queueDuration = ref("--:--:--");
+const skeletonLoading = ref(true);
+const queue = ref([]);
 let player;
 const playerState = ref({
   playerMode: enums.MODES.IDLE,
@@ -164,6 +171,9 @@ onMounted(() => {
   player.on("error", (event) => {
     emit("youtube-media-error", event.data);
   });
+  player.on("ready", function () {
+    requestAnimationFrame(updateCurrentlyPlaying);
+  });
 });
 
 watch(
@@ -180,9 +190,9 @@ onBeforeUnmount(() => {
 
 socket.on("player-update", (newState) => {
   if (newState.playerType === enums.TYPES.VIDEO) {
-    if (newState.playerMode === enums.MODES.PLAYING)
+    if (newState.playerMode === enums.MODES.PLAYING) {
       player.loadVideoById(newState.video.id, newState.timestamp);
-    else player.pauseVideo();
+    } else player.pauseVideo();
   } else if (playerState.value.playerType === enums.TYPES.VIDEO)
     player.stopVideo();
   playerState.value = newState;
@@ -195,9 +205,6 @@ socket.on("new-video-timestamp", async (newStamp) => {
   }
 });
 
-const queueDuration = ref("--:--:--");
-const skeletonLoading = ref(true);
-const queue = ref([]);
 // retrieving the queue and stop skeletonloading
 socket.on("connect", async () => {
   const data = await new Promise((resolve) => {
@@ -214,4 +221,22 @@ socket.on("queue-update", (newQueue) => {
   queueDuration.value = newQueue.duration;
   queue.value = newQueue.queue;
 });
+
+function updateCurrentlyPlaying() {
+  if (player) {
+    // let percentage = (player.getCurrentTime() / player.getDuration()) * 100;
+    player.getCurrentTime().then((time) => {
+      player.getDuration().then((duration) => {
+        let percentage;
+        if (duration > 0) percentage = (time / duration) * 100;
+        if (Number(percentage)) {
+          const bar = document.getElementById("progress-bar");
+          console.log(percentage);
+          bar.style.width = `${percentage}%`;
+        }
+        requestAnimationFrame(updateCurrentlyPlaying);
+      });
+    });
+  }
+}
 </script>
