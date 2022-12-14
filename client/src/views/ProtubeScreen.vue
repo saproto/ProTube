ab50e239eb99d29015e3fbfac7fa6ae16ff854ff
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2022-12-14 21:33:54 +0100

    Make an on-screen queue and make the currently playing have an animated bar (#59)
    * started implementing the on screen queue
    
    * Redid queue
    
    * implement progress bar on currently playing
    
    * remove the currentlyplaying from the visible queue
    
    * Removed socket.on and fixed the width of the Visit protu.be
    
    * remove console.log
    
    * fix SetCurrentvideo and remove unneccesary variable
    
    * Fixed setCurrentVideo and made the queueProgress use Vue
    
    * minor css border rounding
    
    Co-authored-by: Menkveld <menke.veerman@gmail.com>

--- client/src/views/ProtubeScreen.vue
+++ client/src/views/ProtubeScreen.vue
@@ -10,23 +10,8 @@
           {{ screenCode }}
         </div>
       </div>
-      <div
-        v-show="playerState.playerMode !== enums.MODES.IDLE"
-        class="dark:bg-proto_secondary_gray-dark ml-auto mr-4 rounded-lg bg-white px-4 py-2 font-medium text-gray-900 shadow-lg ring-1 ring-black ring-opacity-5 dark:text-gray-50">
-        <div class="mr-1 text-sm text-gray-600 dark:text-gray-300">
-          Now playing:
-        </div>
-        <span class="">
-          {{ playerState.video.title }}
-        </span>
-        <div class="mt-1">
-          <span class="text-sm text-gray-600 dark:text-gray-300">
-            Added by:
-          </span>
-          {{ playerState.video?.user?.name }}
-        </div>
-      </div>
     </div>
+
     <div
       v-if="playerState.playerMode === enums.MODES.IDLE"
       class="grid min-h-screen place-items-center">
@@ -47,6 +32,84 @@
       <div :id="playerID" class="min-h-screen w-full" />
     </div>
   </div>
+  <div class="absolute bottom-0 mb-1 w-screen rounded-lg">
+    <div class="flex justify-between">
+      <div
+        class="border-proto_blue dark:bg-proto_secondary_gray-dark ml-4 mb-1 max-w-[12%] rounded-lg border-l-4 bg-white p-1 px-4 py-2 font-medium text-gray-900 opacity-80 shadow-lg ring-1 ring-black ring-opacity-5 dark:text-gray-50">
+        Queue: {{ queueDuration }}
+      </div>
+      <div
+        class="border-proto_blue dark:bg-proto_secondary_gray-dark mr-4 mb-1 max-w-[12%] rounded-lg border-r-4 bg-white p-1 px-4 py-2 font-medium text-gray-900 opacity-80 shadow-lg ring-1 ring-black ring-opacity-5 dark:text-gray-50">
+        Visit www.protu.be!
+      </div>
+    </div>
+    <div class="flex flex-1 gap-1 overflow-hidden pl-3">
+      <div
+        v-for="(video, index) in queue.slice(0, 5)"
+        :video="video"
+        :index="index"
+        :key="video.id"
+        :id="index === 0 ? 'current-video' : ''"
+        class="mb-1 inline-block w-1/5 rounded-lg p-1">
+        <div
+          :style="{ background: `url(${video.thumbnail.url})` }"
+          style="
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center center;
+          "
+          class="border-proto_blue group relative col-span-1 inline-block flex h-full w-full flex-1 overflow-hidden rounded-sm rounded-lg border-l-4 text-center shadow">
+          <div
+            :style="index === 0 ? `width:${queueProgress}%;` : 'width:0%'"
+            class="absolute h-full bg-white opacity-70" />
+          <div
+            class="rounded-m relative flex w-full flex-col rounded-r-lg border-t border-b border-r border-gray-400 bg-white/70 px-8 py-4 duration-200 dark:border-gray-800/80 dark:bg-stone-800/80">
+            <h3
+              class="text-md z-1 h-[2rem] truncate text-left font-bold text-gray-800 dark:text-stone-300">
+              {{ video.title }}
+            </h3>
+            <ul
+              class="fa-ul mt-auto ml-5 w-full text-sm font-medium text-gray-900 dark:text-stone-300">
+              <li
+                class="justify-bottom mt-auto flex flex-1 text-right align-bottom">
+                <span class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-user" fixed-width>
+                  </font-awesome-icon>
+                </span>
+                <span class="truncate">
+                  {{ video.user.name }}
+                </span>
+              </li>
+              <li class="flex flex-1 text-right">
+                <span class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-microphone" fixed-width>
+                  </font-awesome-icon>
+                </span>
+                <span class="truncate">
+                  {{ video.channel }}
+                </span>
+              </li>
+              <li class="flex flex-1 text-right">
+                <span class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-microphone" fixed-width>
+                  </font-awesome-icon>
+                </span>
+              </li>
+              <li class="flex flex-1 text-right">
+                <span class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-clock fa-li" fixed-width>
+                  </font-awesome-icon>
+                </span>
+                <span class="truncate">
+                  {{ video.durationFormatted }}
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <ReconnectionHandler
     v-if="screenCode === -1"
     :socket="socket"
@@ -62,6 +125,9 @@ import YoutubePlayer from "youtube-player";
 import enums from "@/js/Enums";
 
 const playerID = "player-" + Math.random();
+const queueDuration = ref("--:--:--");
+const queueProgress = ref(0);
+const queue = ref([]);
 let player;
 const playerState = ref({
   playerMode: enums.MODES.IDLE,
@@ -121,18 +187,24 @@ onBeforeUnmount(() => {
 
 socket.on("player-update", (newState) => {
   if (newState.playerType === enums.TYPES.VIDEO) {
-    if (newState.playerMode === enums.MODES.PLAYING)
+    if (newState.playerMode === enums.MODES.PLAYING) {
       player.loadVideoById(newState.video.id, newState.timestamp);
-    else player.pauseVideo();
+    } else player.pauseVideo();
   } else if (playerState.value.playerType === enums.TYPES.VIDEO)
     player.stopVideo();
   playerState.value = newState;
 });
 
 socket.on("new-video-timestamp", async (newStamp) => {
+  queueProgress.value = (newStamp / queue.value[0].duration) * 100;
   if (Math.abs((await player.getCurrentTime()) - newStamp) > 5) {
     player.seekTo(newStamp, true);
     if ((await player.getPlayerState()) === 2) player.playVideo();
   }
+});
+
+socket.on("queue-update", (newQueue) => {
+  queueDuration.value = newQueue.duration;
+  queue.value = newQueue.queue;
 });
 </script>
