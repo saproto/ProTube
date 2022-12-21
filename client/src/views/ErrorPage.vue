5678f9eef0be92e87c3acbb2370cb33bcda037ff
Jessseee <jssvssr2000@gmail.com>
2022-12-21 15:41:29 +0100

    Added reference to github and commitee to error page

--- client/src/views/ErrorPage.vue
+++ client/src/views/ErrorPage.vue
@@ -1,33 +1,41 @@
 <template>
   <div
     class="dark:bg-proto_background_gray-dark grid min-h-screen place-items-center">
-    <!-- snowflake (after use remove 'z-50 class')-->
     <div
-      class="z-50 mx-auto max-w-fit content-center rounded-md bg-white p-8 shadow-md">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-20 w-full text-center text-yellow-600"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-      </svg>
-      {{ codeLookupTable[errorcode] }}
+      class="dark:bg-proto_secondary_gray-dark z-50 mx-auto mx-5 max-w-[450px] content-center rounded-md bg-white p-8 text-center shadow-md dark:text-white">
+      <div class="w-full text-center">
+        <font-awesome-icon
+          icon="fa-solid fa-warning"
+          class="text-yellow-400"
+          size="6x">
+        </font-awesome-icon>
+      </div>
+      <h3 class="mb-3 text-xl font-medium">{{ codeLookupTable[errorcode] }}</h3>
+      <p class="mb-5 text-sm">
+        If you are encountering an unexpected error please let the
+        <a
+          href="https://haveyoutriedturningitoffandonagain.nl"
+          class="text-proto_blue"
+          >HYTTIOAOAc</a
+        >
+        know by creating an issue on
+        <a
+          href="https://github.com/saproto/ProTube/issues/new"
+          class="text-proto_blue"
+          >GitHub</a
+        >.
+      </p>
       <router-link
         :to="routeback"
         v-if="routeback"
         :replace="true"
-        class="bg-proto_blue hover:bg-proto_blue/80 mx-auto mt-2 flex justify-center rounded-md p-2 text-white">
+        class="bg-proto_blue hover:bg-proto_blue/80 mx-auto flex justify-center rounded-md p-2 text-white">
         Go Back
       </router-link>
       <router-link
         :to="'/remote'"
         :replace="true"
-        class="bg-proto_blue hover:bg-proto_blue/80 mx-auto mt-2 flex justify-center rounded-md p-2 text-white">
+        class="bg-proto_blue hover:bg-proto_blue/80 mx-auto flex justify-center rounded-md p-2 text-white">
         Return home
       </router-link>
     </div>
@@ -35,7 +43,8 @@
 </template>
 
 <script setup>
-// See https://en.wikipedia.org/wiki/List_of_HTTP_status_codes for the error codes
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 defineProps({
   errorcode: {
     type: Number,
@@ -43,6 +52,8 @@ defineProps({
   },
   routeback: String,
 });
+
+// See https://en.wikipedia.org/wiki/List_of_HTTP_status_codes for error codes
 const codeLookupTable = {
   404: "404 - Page not found!",
   401: "401 - Unauthorized!",
