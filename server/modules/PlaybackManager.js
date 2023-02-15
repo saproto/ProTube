65c78605f931ea0971ecda8e7c7f288c64ba4bdf
Lint Action <lint-action@samuelmeuli.com>
2023-02-15 21:17:54 +0000

    Fix code style issues with Prettier

--- server/modules/PlaybackManager.js
+++ server/modules/PlaybackManager.js
@@ -119,7 +119,7 @@ exports.toggleType = () => {
     playerType = enums.TYPES.VIDEO;
     try {
       this.playNextVideo();
-    } catch { }
+    } catch {}
     eventBus.emit("player-update");
     return enums.SUCCESS;
   }
