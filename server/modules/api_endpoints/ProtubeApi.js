83950278d1db80edf944681da5ec507deec3aaf2
Menkveld <menke.veerman@gmail.com>
2023-05-09 23:16:53 +0200

    Added update endpoint for laravel

--- server/modules/api_endpoints/ProtubeApi.js
+++ server/modules/api_endpoints/ProtubeApi.js
@@ -10,33 +10,37 @@ this.protubeApi.use(bearerToken());
 this.protubeApi.use(checkBearerToken);
 
 // Endpoint to update the admin status of a user id
-this.protubeApi.post("/updateadmin", async function (req, res) {
+this.protubeApi.post("/updateadmin/:userID(\d+)", async function (req, res) {
+  const userID = parseInt(req.params.userID);
+  
   logger.apiInfo(
-    `Attempt from ${req.hostname} to update the admin status of a user`
+    `Attempt from ${req.hostname} to update the admin status of user ${userID}`
   );
-  // Check if the required data is present and parse it
-  if (!req.body?.user_id || !req.body?.admin_until || !req.body?.admin_from) {
-    logger.apiInfo("Request had incomplete body");
-    return res.send({ success: enums.FAIL, message: "Incomplete body" });
-  }
-  const userID = parseInt(req.body.user_id);
-  const adminFrom = parseInt(req.body.admin_from);
-  const adminUntil = parseInt(req.body.admin_until);
-
   // finding and updating the users admin status in the database
   const user = await User.findByPk(userID);
   if (!user) {
     logger.apiInfo("User is not found!");
     return res.send({ success: enums.FAIL, message: "User not found!" });
   }
-
-  user.admin_until = adminUntil;
-  user.admin_from = adminFrom;
-  await user.save();
-
-  logger.apiInfo(
-    `User ${userID}'s is admin from: ${adminFrom} until: ${adminUntil}`
-  );
+  
+  fetch(
+    `${process.env.LARAVEL_ENDPOINT}/api/protube/userdetails`,
+    {
+      headers: {
+        Authorization: "Bearer " + user.access_token,
+      },
+    }
+  ).then(async (response) => {
+    const userData = await response.json();
+    await User.upsert({
+      id: userData.id,
+      name: userData.name,
+      admin_from: +userData.admin_from,
+      admin_until: +userData.admin_until,
+      refresh_token: refreshToken,
+      access_token: accessToken,
+    });
+  });
   return res.send({ success: enums.SUCCESS });
 });
 
