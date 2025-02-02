6bec86fcf7dbf0f4d693055b10baaa6d78e8bab2
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2025-02-02 17:00:05 +0100

    Consume the new endpoint which returns six photos per album

--- server/modules/socket_endpoints/ScreenSocket.js
+++ server/modules/socket_endpoints/ScreenSocket.js
@@ -4,7 +4,11 @@ const queueManager = require("../QueueManager");
 const playbackManager = require("../PlaybackManager");
 let newPhotoInterval = null;
 let photo = null;
-
+let album={
+  album_name: "",
+  date_taken:"",
+  photos:[]
+}
 endpoint.on("connection", (socket) => {
   logger.screenInfo(
     `Screen connected from ${socket.handshake.address} with socket id ${socket.id}`
@@ -69,18 +73,27 @@ function emitNewPhoto() {
   if (newPhotoInterval === null) {
     newPhotoInterval = setInterval(emitNewPhoto, 10000);
   }
-  fetch(`${process.env.LARAVEL_ENDPOINT}/api/photos/random_photo`)
-    .then((res) => res.json())
-    .then((newPhoto) => {
-      photo = newPhoto;
-      endpoint.emit("photo-update", photo);
-    })
-    .catch((e) => {
-      endpoint.emit("photo-update", {
-        url: "",
-        album_name: "",
-        date_taken: 0,
-        error: e,
-      });
+  if(album.photos.length>0){
+    photo = album.photos.shift()
+    endpoint.emit("photo-update", {
+      url: photo,
+      album_name: album.album_name,
+      date_taken: album.date_taken,
     });
+  }
+  if(album.photos.length===0){
+    fetch(`${process.env.LARAVEL_ENDPOINT}/api/photos/random_album`)
+      .then((res) => res.json())
+      .then((newAlbum) => {
+        album = newAlbum;
+      })
+      .catch((e) => {
+        endpoint.emit("photo-update", {
+          url: "",
+          album_name: "",
+          date_taken: 0,
+          error: e,
+        });
+      });
+  }
 }
