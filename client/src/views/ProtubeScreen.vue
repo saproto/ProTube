97c5f9658509f0ef16c5ac9d4a7c60adb7716cfa
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-04-01 12:22:35 +0200

    Add back in the v-if

--- client/src/views/ProtubeScreen.vue
+++ client/src/views/ProtubeScreen.vue
@@ -63,7 +63,7 @@
       </div>
     </div>
 
-    <div class="dark:text-white">
+    <div v-if="!isPlayingVideo || screenSettings.smallPlayer" class="dark:text-white">
       <div v-if="photo && !photo.error && photo.url !== ''">
         <div class="flex h-screen justify-center overflow-x-hidden p-5">
           <img
